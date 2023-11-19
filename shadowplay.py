433b07f517530c38d0abf1921483a091f1fe312e
Flask <87578456+Failte327@users.noreply.github.com>
2023-11-19 15:10:06 -0700

    added ui for turning on and off recording, not working yet

--- shadowplay.py
+++ shadowplay.py
@@ -2,6 +2,19 @@ import pyautogui
 import cv2
 import numpy as np
 import time
+from tkinter import *
+from tkinter import ttk
+
+global recording
+recording = True
+
+def start_recording():
+    recording = True
+    print(f"recording = {recording}")
+
+def stop_recording():
+    recording = False
+    print(f"recording = {recording}")
 
 timestamp = time.time()
 screen_size = (1920, 1080)
@@ -10,9 +23,18 @@ output_filename = f"{timestamp}.avi"
 
 fourcc = cv2.VideoWriter_fourcc(*"XVID")
 out = cv2.VideoWriter(output_filename, fourcc, fps, screen_size)
-print("Starting recording")
+print("Starting program")
 
-while True:
+root = Tk()
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+ttk.Label(frm, text="DAoC Recorder").grid(column=0, row=0)
+ttk.Button(frm, text="Start Recording", command=start_recording()).grid(column=1, row=0)
+ttk.Button(frm, text="Stop Recording", command=stop_recording()).grid(column=1, row=1)
+
+root.mainloop()
+
+while recording == True:
     # Capture screen content
     frame = pyautogui.screenshot()
     frame = np.array(frame)
@@ -23,9 +45,7 @@ while True:
     # Write the frame to the video file
     out.write(frame)
 
-    # Stop recording when the user presses the 'q' key (not currently working)
-    if cv2.waitKey(1) == ord("q"):
-        print("Stopping recording")
+    if (recording == False):
         break
 
 # Release the VideoWriter and close the OpenCV windows
